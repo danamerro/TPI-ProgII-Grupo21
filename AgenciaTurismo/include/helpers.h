15f590ed099e0a9f2df7907095faad306327f154
gabriela canela <gmdreher21@gmail.com>
2026-06-19 12:39:59 -0300

    Se elimina funcion helper para implementar en otro momento

--- AgenciaTurismo/include/helpers.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-bool cargarYValidar(int &variable, const char* mensajeError = "Ingrese solo numeros enteros.");
-
-bool cargarYValidar(float &variable, const char* mensajeError = "Ingrese solo numeros.");
-
-bool cargarYValidar(char* cadena, int tamano, const char* mensajeError = "Ingrese solo caracteres");
